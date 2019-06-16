35a22f6c84982d0efe2c90d763888ebe4eb93b1a
Jonathan Cervas <jcervas@uci.edu>
2019-06-16 13:47:36 -0700

    Update Plots.R

--- R/Plots.R
+++ R/Plots.R
@@ -10,344 +10,335 @@ cat(
 		CREATING PLOTS . . . . . . . . . . . . . . . . . . . . . 
 •••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••\n")
 
-# % -----------------------------------------------------------------
-yrs <- seq(2012,2016,2)
-pdf(paste0("Figures/fig_sims_actual_density.pdf"), 
-	width = 5, 
-	height = 6)
-par(mfrow=c(3,1), 
-	oma= c(3,1.5,0,0), 
-	mar=c(3,1,1,1), 
-	mgp= c(2,1,0))
-for (k in 1:3)
-	{		
-		sims.tmp.yrs <- sim.election(
-			votes = default.unc(house.2016.votes), 
-			center = default.unc(get(paste0("house.", yrs[k],".votes"))), 
-			yr=yrs[k], sims=1000, 
-			sigma=sigma)
+# # % -----------------------------------------------------------------
+# yrs <- seq(2012,2016,2)
+# pdf(paste0("Figures/fig_sims_actual_density.pdf"), 
+# 	width = 5, 
+# 	height = 6)
+# par(mfrow=c(3,1), 
+# 	oma= c(3,1.5,0,0), 
+# 	mar=c(3,1,1,1), 
+# 	mgp= c(2,1,0))
+# for (k in 1:3)
+# 	{		
+# 		sims.tmp.yrs <- sim.election(
+# 			votes = default.unc(house.2016.votes), 
+# 			center = default.unc(get(paste0("house.", yrs[k],".votes"))), 
+# 			yr=yrs[k], sims=1000, 
+# 			sigma=sigma)
 
-		x <- (unlist(lapply(sims.tmp.yrs, seats))) # Vector of Seats from Simulations for each year
-		x.sd <- sd(x)
-		x.mean <- mean(x)
-		a <- density(x, na.rm=TRUE, from=x.mean-x.sd, to=x.mean+x.sd)
-		area <- abs(sum(a$y)*(a$x[1]-a$x[2]))
-		# twec <- sum(d$ecvotes[d$p>.5])/sum(d$ecvotes)
-		e1 <- density(x, na.rm=TRUE, adjust=2)
+# 		x <- (unlist(lapply(sims.tmp.yrs, seats))) # Vector of Seats from Simulations for each year
+# 		x.sd <- sd(x)
+# 		x.mean <- mean(x)
+# 		a <- density(x, na.rm=TRUE, from=x.mean-x.sd, to=x.mean+x.sd)
+# 		area <- abs(sum(a$y)*(a$x[1]-a$x[2]))
+# 		# twec <- sum(d$ecvotes[d$p>.5])/sum(d$ecvotes)
+# 		e1 <- density(x, na.rm=TRUE, adjust=2)
 		
-	plot(e1, xlim=c(0,1), ylim=c(0,15), main=yrs[k], xlab = "", ylab = "", xaxt = "n", yaxt = "n")
-		abline(v=seq(0,1,0.1), 
-			lty=2, 
-			col="gray95")
-		axis(
-			side=1, 
-			las=1, 
-			at=seq(0,1,0.1), 
-			labels=paste0(seq(0,100,10),"%"), 
-			cex.axis=0.85)
-		mtext(side=1, 
-			line=3, 
-			ifelse(k %in% c(3, 8), "Republican Share of Seats", ""), 
-			cex=0.65)
-		x4 <- min(which(e1$x>x.mean-x.sd))
-		x2 <- min(which(e1$x>x.mean+x.sd))
-	with(e1, 
-		polygon(
-			x = c(x[c(x2,x2:x4,x4)]),  
-			y = c(0, y[x2:x4], 0), 
-			col = "gray60", 
-			angle = 225, 
-			density = 40, 
-			border = NA))
-	with(e1, 
-		polygon(
-			x = c(x[c(x2,x2:x4,x4)]),  
-			y = c(0, y[x2:x4], 0), 
-			col = "gray60", 
-			angle = 135, 
-			density = 40, 
-			border = NA))
-		rect(
-			-.1,-1,1.1,0, 
-			col="black")
-		points(e1, 
-			type="l")
-		actual.seats <- seats(default.unc(get(paste0("house.", yrs[k], ".votes"))))
-			if (k == 1) arrows(
-				x0 = actual.seats - 0.03, 
-				x1 = actual.seats - 0.1, 
-				y0 = 7,
-				y1 = 7, 
-				angle=40, 
-				length=0.05, 
-				code=1, 
-				lty=1, 
-				col="gray10", 
-				lwd=1)
-			abline(v = actual.seats, lty=2, col="gray5")
-	if (k == 1) text(actual.seats - 0.1, 7, "Actual Congressional \n Results", pos=2)
-	}
-	dev.off()
+# 	plot(e1, xlim=c(0,1), ylim=c(0,15), main=yrs[k], xlab = "", ylab = "", xaxt = "n", yaxt = "n")
+# 		abline(v=seq(0,1,0.1), 
+# 			lty=2, 
+# 			col="gray95")
+# 		axis(
+# 			side=1, 
+# 			las=1, 
+# 			at=seq(0,1,0.1), 
+# 			labels=paste0(seq(0,100,10),"%"), 
+# 			cex.axis=0.85)
+# 		mtext(side=1, 
+# 			line=3, 
+# 			ifelse(k %in% c(3, 8), "Republican Share of Seats", ""), 
+# 			cex=0.65)
+# 		x4 <- min(which(e1$x>x.mean-x.sd))
+# 		x2 <- min(which(e1$x>x.mean+x.sd))
+# 	with(e1, 
+# 		polygon(
+# 			x = c(x[c(x2,x2:x4,x4)]),  
+# 			y = c(0, y[x2:x4], 0), 
+# 			col = "gray60", 
+# 			angle = 225, 
+# 			density = 40, 
+# 			border = NA))
+# 	with(e1, 
+# 		polygon(
+# 			x = c(x[c(x2,x2:x4,x4)]),  
+# 			y = c(0, y[x2:x4], 0), 
+# 			col = "gray60", 
+# 			angle = 135, 
+# 			density = 40, 
+# 			border = NA))
+# 		rect(
+# 			-.1,-1,1.1,0, 
+# 			col="black")
+# 		points(e1, 
+# 			type="l")
+# 		actual.seats <- seats(default.unc(get(paste0("house.", yrs[k], ".votes"))))
+# 			if (k == 1) arrows(
+# 				x0 = actual.seats - 0.03, 
+# 				x1 = actual.seats - 0.1, 
+# 				y0 = 7,
+# 				y1 = 7, 
+# 				angle=40, 
+# 				length=0.05, 
+# 				code=1, 
+# 				lty=1, 
+# 				col="gray10", 
+# 				lwd=1)
+# 			abline(v = actual.seats, lty=2, col="gray5")
+# 	if (k == 1) text(actual.seats - 0.1, 7, "Actual Congressional \n Results", pos=2)
+# 	}
+# 	dev.off()
 
-Figure(
-	path="Figures/fig_sims_actual_density.pdf", 
-	caption="Comparing the Simulated Elections with US Congressional Elections (PA)", 
-	label="fig:densityplots_congressional", 
-	footnote="Plots based on the 2016 Five Election Composite data centered at the vote share for each year. All percentages in terms of Republican share of the two-party vote from the composite measure of five state-wide races in 2016. Shaded area contains one standard deviation on either side of the mean, representing 68\\% of the simulated seat percentages.")
+# Figure(
+# 	path="Figures/fig_sims_actual_density.pdf", 
+# 	caption="Comparing the Simulated Elections with US Congressional Elections (PA)", 
+# 	label="fig:densityplots_congressional", 
+# 	footnote="Plots based on the 2016 Five Election Composite data centered at the vote share for each year. All percentages in terms of Republican share of the two-party vote from the composite measure of five state-wide races in 2016. Shaded area contains one standard deviation on either side of the mean, representing 68\\% of the simulated seat percentages.")
 
-
-
-
-
-
-
-pdf(paste0("Figures/fig_sims_2016_maps_density.pdf"), 
-	width = 8, 
-	height = 10)
-par(mfrow=c(4,2), 
-	oma= c(3,1.5,0,0), 
-	mar=c(3,1,1,1), 
-	mgp= c(2,1,0))
-for (k in 1:length(maps.sims.seats.2016))
-	{		
-		x <- do.call(rbind, maps.sims.seats.2016[k])
-		x.sd <- sd(x)
-		x.mean <- mean(x)
-		a <- density(x, na.rm=TRUE, from=x.mean-x.sd, to=x.mean+x.sd)
-		area <- abs(sum(a$y)*(a$x[1]-a$x[2]))
-		# twec <- sum(d$ecvotes[d$p>.5])/sum(d$ecvotes)
-		e1 <- density(x, na.rm=TRUE, adjust=2)
+# •••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+# •••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+# pdf(paste0("Figures/fig_sims_2016_maps_density.pdf"), 
+# 	width = 8, 
+# 	height = 10)
+# par(mfrow=c(4,2), 
+# 	oma= c(3,1.5,0,0), 
+# 	mar=c(3,1,1,1), 
+# 	mgp= c(2,1,0))
+# for (k in 1:length(maps.sims.seats.2016))
+# 	{		
+# 		x <- do.call(rbind, maps.sims.seats.2016[k])
+# 		x.sd <- sd(x)
+# 		x.mean <- mean(x)
+# 		a <- density(x, na.rm=TRUE, from=x.mean-x.sd, to=x.mean+x.sd)
+# 		area <- abs(sum(a$y)*(a$x[1]-a$x[2]))
+# 		# twec <- sum(d$ecvotes[d$p>.5])/sum(d$ecvotes)
+# 		e1 <- density(x, na.rm=TRUE, adjust=2)
 		
-	plot(e1, xlim=c(0,1), ylim=c(0,10), main=plan_names[k], xlab = "", ylab = "", xaxt = "n", yaxt = "n")
-		abline(v=seq(0,1,0.1), 
-			lty=2, 
-			col="gray95")
-		axis(
-			side=1, 
-			las=1, 
-			at=seq(0,1,0.1), 
-			labels=paste0(seq(0,100,10),"%"), 
-			cex.axis=0.85)
-		mtext(side=1, 
-			line=3, 
-			ifelse(k %in% c(7, 8), "Republican Share of Seats", ""), 
-			cex=0.65)
-		x4 <- min(which(e1$x>x.mean-x.sd))
-		x2 <- min(which(e1$x>x.mean+x.sd))
-	with(e1, 
-		polygon(
-			x = c(x[c(x2,x2:x4,x4)]),  
-			y = c(0, y[x2:x4], 0), 
-			col = "gray60", 
-			angle = 225, 
-			density = 40, 
-			border = NA))
-	with(e1, 
-		polygon(
-			x = c(x[c(x2,x2:x4,x4)]),  
-			y = c(0, y[x2:x4], 0), 
-			col = "gray60", 
-			angle = 135, 
-			density = 40, 
-			border = NA))
-		rect(
-			-.1,-1,1.1,0, 
-			col="black")
-		points(e1, 
-			type="l")
-		actual.seats <- seats(default.unc(get(paste0("house.2016.votes"))))
-			if (k == 1) arrows(
-				x0 = actual.seats - 0.03, 
-				x1 = actual.seats - 0.1, 
-				y0 = 7,
-				y1 = 7, 
-				angle=40, 
-				length=0.05, 
-				code=1, 
-				lty=1, 
-				col="gray10", 
-				lwd=1)
-			abline(v = actual.seats, lty=2, col="gray5")
-	if (k == 1) text(actual.seats - 0.1, 7, "Actual Congressional \n Results", pos=2)
-				legend("topright", 
-			legend=
-				c("SEAT SHARE ",
-					paste0("  Average: ", 
-					percent(mean(unlist(maps.sims.seats.2016[k])))), 
-				paste0("  Median: ", 
-					percent(median(unlist(maps.sims.seats.2016[k]))))),
-				 bty="n", 
-				 box.lwd=0,
-				 cex=0.75)
-		}
-	dev.off()
+# 	plot(e1, xlim=c(0,1), ylim=c(0,10), main=plan_names[k], xlab = "", ylab = "", xaxt = "n", yaxt = "n")
+# 		abline(v=seq(0,1,0.1), 
+# 			lty=2, 
+# 			col="gray95")
+# 		axis(
+# 			side=1, 
+# 			las=1, 
+# 			at=seq(0,1,0.1), 
+# 			labels=paste0(seq(0,100,10),"%"), 
+# 			cex.axis=0.85)
+# 		mtext(side=1, 
+# 			line=3, 
+# 			ifelse(k %in% c(7, 8), "Republican Share of Seats", ""), 
+# 			cex=0.65)
+# 		x4 <- min(which(e1$x>x.mean-x.sd))
+# 		x2 <- min(which(e1$x>x.mean+x.sd))
+# 	with(e1, 
+# 		polygon(
+# 			x = c(x[c(x2,x2:x4,x4)]),  
+# 			y = c(0, y[x2:x4], 0), 
+# 			col = "gray60", 
+# 			angle = 225, 
+# 			density = 40, 
+# 			border = NA))
+# 	with(e1, 
+# 		polygon(
+# 			x = c(x[c(x2,x2:x4,x4)]),  
+# 			y = c(0, y[x2:x4], 0), 
+# 			col = "gray60", 
+# 			angle = 135, 
+# 			density = 40, 
+# 			border = NA))
+# 		rect(
+# 			-.1,-1,1.1,0, 
+# 			col="black")
+# 		points(e1, 
+# 			type="l")
+# 		actual.seats <- seats(default.unc(get(paste0("house.2016.votes"))))
+# 			if (k == 1) arrows(
+# 				x0 = actual.seats - 0.03, 
+# 				x1 = actual.seats - 0.1, 
+# 				y0 = 7,
+# 				y1 = 7, 
+# 				angle=40, 
+# 				length=0.05, 
+# 				code=1, 
+# 				lty=1, 
+# 				col="gray10", 
+# 				lwd=1)
+# 			abline(v = actual.seats, lty=2, col="gray5")
+# 	if (k == 1) text(actual.seats - 0.1, 7, "Actual Congressional \n Results", pos=2)
+# 				legend("topright", 
+# 			legend=
+# 				c("SEAT SHARE ",
+# 					paste0("  Average: ", 
+# 					percent(mean(unlist(maps.sims.seats.2016[k])))), 
+# 				paste0("  Median: ", 
+# 					percent(median(unlist(maps.sims.seats.2016[k]))))),
+# 				 bty="n", 
+# 				 box.lwd=0,
+# 				 cex=0.75)
+# 		}
+# 	dev.off()
 
-Figure(
-	path="Figures/fig_sims_2016_maps_density.pdf", 
-	caption="Simulated 2016 Elections by Plan", 
-	label="fig:density2016", 
-	footnote="Plots based on the 2016 Five Election Composite data centered at the 2016 vote share. All percentages in terms of Republican share of the two-party vote from the composite measure of five state-wide races in 2016. Shaded area contains one standard deviation on either side of the mean, representing 68\\% of the simulated seat percentages.")
-
-
-
-
-
+# Figure(
+# 	path="Figures/fig_sims_2016_maps_density.pdf", 
+# 	caption="Simulated 2016 Elections by Plan", 
+# 	label="fig:density2016", 
+# 	footnote="Plots based on the 2016 Five Election Composite data centered at the 2016 vote share. All percentages in terms of Republican share of the two-party vote from the composite measure of five state-wide races in 2016. Shaded area contains one standard deviation on either side of the mean, representing 68\\% of the simulated seat percentages.")
+# •••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+# •••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
 
 
 radius <- function(x) perimeter(x) / (2 * 3.1415926535)
-
-
 # COMPACTNESS EXAMPLE: DISTRICT 6 OF THE COURT ADOPTED court PLAN AND DISTRICT 7 OF THE 2011 PLAN
 # % =================================================================
 # % -- FIGURE -- FIGURE -- FIGURE -- FIGURE -- FIGURE -- FIGURE --  %
 # % -----------------------------------------------------------------
-pdf(paste0("Figures/fig_compactness.pdf"), width = 6, height = 6)
-	par(
-		mfrow = c(2, 2), 
-		mar = c(0, 0, 0, 0), 
-		oma = c(0, 0, 0, 0), 
-		mai = c(0, 0, 0.4, 0), 
-		xpd = NA, 
-		pty = "s")
-# Reock
-	plot(
-		boundingcircle(court.cd.6.boundary), main = "")
-		rect(
-			1 * par("usr")[1], 
-			(1 * par("usr")[3]), 
-			0.995 * par("usr")[2], 
-			(1 * par("usr")[4]), 
-			density = NULL, 
-			angle = 45, 
-			col = "gray70", 
-			border = NA,
-			lty = par("lty"), 
-			lwd = par("lwd"), 
-			xpd = NA)
+# pdf(paste0("Figures/fig_compactness.pdf"), width = 6, height = 6)
+# 	par(
+# 		mfrow = c(2, 2), 
+# 		mar = c(0, 0, 0, 0), 
+# 		oma = c(0, 0, 0, 0), 
+# 		mai = c(0, 0, 0.4, 0), 
+# 		xpd = NA, 
+# 		pty = "s")
+# # Reock
+# 	plot(
+# 		boundingcircle(court.cd.6.boundary), main = "")
+# 		rect(
+# 			1 * par("usr")[1], 
+# 			(1 * par("usr")[3]), 
+# 			0.995 * par("usr")[2], 
+# 			(1 * par("usr")[4]), 
+# 			density = NULL, 
+# 			angle = 45, 
+# 			col = "gray70", 
+# 			border = NA,
+# 			lty = par("lty"), 
+# 			lwd = par("lwd"), 
+# 			xpd = NA)
 
-	plot(
-		boundingcircle(court.cd.6.boundary), 
-		col = "white", 
-		add = T)
+# 	plot(
+# 		boundingcircle(court.cd.6.boundary), 
+# 		col = "white", 
+# 		add = T)
 
-	plot(
-		court.cd.6.boundary, 
-		add = T, 
-		col = "gray30", 
-		border = "gray30")
-
-
-# Plot Polsby - Popper
-	r <- radius(court.cd.6.boundary)
-		circle.new(
-			centroid.owin(court.cd.6.boundary)$x, 
-			centroid.owin(court.cd.6.boundary)$y, 
-			r, 
-			main = "", 
-			add = F)
-		rect(
-			1.0015 * par("usr")[1], 
-			(1 * par("usr")[3]), 
-			1 * par("usr")[2], 
-			(1 * par("usr")[4]), 
-			density = NULL, 
-			angle = 45, 
-			col = "gray70", 
-			border = NA,
-		    lty = par("lty"), 
-		    lwd = par("lwd"), 
-		    xpd = NA)
-		circle(
-			centroid.owin(court.cd.6.boundary)$x, 
-			centroid.owin(court.cd.6.boundary)$y, 
-			r, 
-			col = "white")
-
-	plot(
-		court.cd.6.boundary, 
-		col = "gray30", 
-		border = "gray30", 
-		add = T)
-
-# •••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
-	# Plot Second plan
-		# Reock
-	plot(
-		boundingcircle(enacted.cd.7.shp), 
-		main = "Reock")
-		rect(
-			1 * par("usr")[1], 
-			(1 * par("usr")[3]), 
-			0.995 * par("usr")[2], 
-			(1 * par("usr")[4]), 
-			density = NULL, 
-			angle = 45, 
-			col = "gray70", 
-			border = NA,
-			lty = par("lty"), 
-			lwd = par("lwd"), 
-			xpd = NA)
-	plot(
-		boundingcircle(enacted.cd.7.shp), 
-		col = "white", 
-		add = T)
-
-	plot(
-		enacted.cd.7.shp, 
-		add = T, 
-		col = "gray30", 
-		border = "gray30")
+# 	plot(
+# 		court.cd.6.boundary, 
+# 		add = T, 
+# 		col = "gray30", 
+# 		border = "gray30")
 
 
-	# Plot Polsby - Popper
-	r <- radius(enacted.cd.7.shp) 
-		circle.new(
-			centroid.owin(enacted.cd.7.shp)$x, 
-			centroid.owin(enacted.cd.7.shp)$y, 
-			r, 
-			main = "Polsby-Popper", 
-			add = F)
-		rect(
-			1.0015 * par("usr")[1], 
-			(1 * par("usr")[3]), 1 * par("usr")[2], 
-			(1 * par("usr")[4]), 
-			density = NULL, 
-			angle = 45, 
-			col = "gray70", 
-			border = NA,
-		    lty = par("lty"), 
-		    lwd = par("lwd"), 
-		    xpd = NA)
-		circle(
-			centroid.owin(enacted.cd.7.shp)$x, 
-			centroid.owin(enacted.cd.7.shp)$y, 
-			r, 
-			col = "white")
+# # Plot Polsby - Popper
+# 	r <- radius(court.cd.6.boundary)
+# 		circle.new(
+# 			centroid.owin(court.cd.6.boundary)$x, 
+# 			centroid.owin(court.cd.6.boundary)$y, 
+# 			r, 
+# 			main = "", 
+# 			add = F)
+# 		rect(
+# 			1.0015 * par("usr")[1], 
+# 			(1 * par("usr")[3]), 
+# 			1 * par("usr")[2], 
+# 			(1 * par("usr")[4]), 
+# 			density = NULL, 
+# 			angle = 45, 
+# 			col = "gray70", 
+# 			border = NA,
+# 		    lty = par("lty"), 
+# 		    lwd = par("lwd"), 
+# 		    xpd = NA)
+# 		circle(
+# 			centroid.owin(court.cd.6.boundary)$x, 
+# 			centroid.owin(court.cd.6.boundary)$y, 
+# 			r, 
+# 			col = "white")
 
-	plot(
-		enacted.cd.7.shp, 
-		col = "gray30", 
-		border = "gray30", 
-		add = T)
+# 	plot(
+# 		court.cd.6.boundary, 
+# 		col = "gray30", 
+# 		border = "gray30", 
+# 		add = T)
+
+# # •••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+# 	# Plot Second plan
+# 		# Reock
+# 	plot(
+# 		boundingcircle(enacted.cd.7.shp), 
+# 		main = "Reock")
+# 		rect(
+# 			1 * par("usr")[1], 
+# 			(1 * par("usr")[3]), 
+# 			0.995 * par("usr")[2], 
+# 			(1 * par("usr")[4]), 
+# 			density = NULL, 
+# 			angle = 45, 
+# 			col = "gray70", 
+# 			border = NA,
+# 			lty = par("lty"), 
+# 			lwd = par("lwd"), 
+# 			xpd = NA)
+# 	plot(
+# 		boundingcircle(enacted.cd.7.shp), 
+# 		col = "white", 
+# 		add = T)
+
+# 	plot(
+# 		enacted.cd.7.shp, 
+# 		add = T, 
+# 		col = "gray30", 
+# 		border = "gray30")
 
 
-	title(
-		"court Plan\n District 6", 
-		outer = TRUE, 
-		line = -5, 
-		cex.main = 0.7)
-	title(
-		"2011 Plan Plan\n District 7", 
-		outer = TRUE, 
-		line = -23, 
-		cex.main = 0.7)
-dev.off()
-  # write.csv(do.call("rbind", plan_compactness),  "Side Projects/PA Redistricting/Data/Plan Comparisons/compactness.csv")
-Figure(
-	path="Figures/fig_compactness.pdf", 
-	caption="Compactness Example: District 6 of the Court Adopted court Plan and District 7 of the 2011 Plan", 
-	label="fig:compact", 
-	footnote="District 7 of the Enacted plan is recognized as the ``Goofy kicking Donald Duck'', while District 6 of the court map has the same geographic center.")
+# 	# Plot Polsby - Popper
+# 	r <- radius(enacted.cd.7.shp) 
+# 		circle.new(
+# 			centroid.owin(enacted.cd.7.shp)$x, 
+# 			centroid.owin(enacted.cd.7.shp)$y, 
+# 			r, 
+# 			main = "Polsby-Popper", 
+# 			add = F)
+# 		rect(
+# 			1.0015 * par("usr")[1], 
+# 			(1 * par("usr")[3]), 1 * par("usr")[2], 
+# 			(1 * par("usr")[4]), 
+# 			density = NULL, 
+# 			angle = 45, 
+# 			col = "gray70", 
+# 			border = NA,
+# 		    lty = par("lty"), 
+# 		    lwd = par("lwd"), 
+# 		    xpd = NA)
+# 		circle(
+# 			centroid.owin(enacted.cd.7.shp)$x, 
+# 			centroid.owin(enacted.cd.7.shp)$y, 
+# 			r, 
+# 			col = "white")
+
+# 	plot(
+# 		enacted.cd.7.shp, 
+# 		col = "gray30", 
+# 		border = "gray30", 
+# 		add = T)
+
+
+# 	title(
+# 		"court Plan\n District 6", 
+# 		outer = TRUE, 
+# 		line = -5, 
+# 		cex.main = 0.7)
+# 	title(
+# 		"2011 Plan Plan\n District 7", 
+# 		outer = TRUE, 
+# 		line = -23, 
+# 		cex.main = 0.7)
+# dev.off()
+#   # write.csv(do.call("rbind", plan_compactness),  "Side Projects/PA Redistricting/Data/Plan Comparisons/compactness.csv")
+# Figure(
+# 	path="Figures/fig_compactness.pdf", 
+# 	caption="Compactness Example: District 6 of the Court Adopted court Plan and District 7 of the 2011 Plan", 
+# 	label="fig:compact", 
+# 	footnote="District 7 of the Enacted plan is recognized as the ``Goofy kicking Donald Duck'', while District 6 of the court map has the same geographic center.")
   
 
 
@@ -385,7 +376,7 @@ par(mar=c(0,0,0,0))
 plot(.5,.5, xlim = c(.4, .7), ylim = c(.4, .7), type = "n", axes = F, xlab = "", ylab = "")#call empty plot to keep on same scale
 text(.55, .55, " ", cex = 1.2, srt = 0, xpd = NA)
 plot(.5,.5, xlim = c(.4, .7), ylim = c(.4, .7), type = "n", axes = F, xlab = "", ylab = "")#call empty plot to keep on same scale
-text(.55, .55, "Southeast Philadelphia", cex = 1.2, srt = 0, xpd = NA)
+text(.55, .55, "Philadelphia", cex = 1.2, srt = 0, xpd = NA)
 for (plan in 1:4){
 par(mar=c(0,0.5,0,0))
 plot(.5,.5, xlim = c(.4, .7), ylim = c(.4, .7), type = "n", axes = F, xlab = "", ylab = "")#call empty plot to keep on same scale
